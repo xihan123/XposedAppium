fe7583491f51cdba881692545b7882c8e7e93963
xihan123 <srxqzxs@vip.qq.com>
2022-07-03 21:11:45 +0800

    update

--- XposedAppiumLib/src/main/java/com/zhenxi/Superappium/WebViewHelper.java
+++ XposedAppiumLib/src/main/java/com/zhenxi/Superappium/WebViewHelper.java
@@ -6,7 +6,7 @@ import android.util.Log;
 import android.webkit.ValueCallback;
 import android.webkit.WebView;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 
 import java.util.HashMap;
 import java.util.HashSet;
